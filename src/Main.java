204a41fb2f5f78db659ad4447abbfcd8908b6dc2
kamil <kamil.hamidullin@gmail.com>
2014-04-30 11:07:34 +0400

    sort result by count and after by login

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     static OutputStreamProvider outputStreamProvider = new DefaultOutputStreamProvider();
 
 	public static void main(String[] args) {
-		IOApp app = new IOApp(inputStreamProviderFactory(args), outputStreamProvider);
+		IOApp app = new IOApp(
+                inputStreamProviderFactory(args),
+                outputStreamProvider
+        );
+
+        //run app
 		app.process();
 	}
 
@@ -19,6 +21,9 @@ public class Main {
 		return new DefaultInputStreamProvider();
 	}
 
+    /**
+     * simple logs processor class
+     */
     public static class IOApp {
 	    private InputStream inputStream;
 	    private OutputStream outputStream;
@@ -37,19 +42,33 @@ public class Main {
            return outputStream;
         }
 
+        /**
+         * write notice message
+         * @param err
+         * @throws IOException
+         */
         private void writeNotice(String err) throws IOException {
            String message = String.format("notice: %s", err);
-           this.outputStream.write(message.getBytes());
+            getOutputStream().write(message.getBytes());
         }
 
+        /**
+         * write result message
+         * @param result
+         * @throws IOException
+         */
         private void writeResult(Map<String, Integer> result) throws IOException {
             StringBuilder resultString = new StringBuilder();
             resultString.append("-------- result ---------\n");
             for(Map.Entry<String, Integer> entry :  result.entrySet()) {
-                resultString.append(String.format("%s - %s\n", entry.getKey(), entry.getValue()));
+                String login = entry.getKey();
+                String capitalizeLogin = login.substring(0, 1).toUpperCase()
+                        + login.substring(1);
+
+                resultString.append(String.format("%s - %s\n", capitalizeLogin, entry.getValue()));
             }
 
-            this.outputStream.write(resultString.toString().getBytes());
+            getOutputStream().write(resultString.toString().getBytes());
         }
 
         /**
@@ -68,9 +87,6 @@ public class Main {
             return logins;
         }
 
-
-
-
         public void process() {
             InputStreamReader inputStreamReader = new InputStreamReader(getInputStream());
             StringBuilder sb = new StringBuilder();
@@ -85,7 +101,13 @@ public class Main {
                } else {
                    HashMap<String, Integer> result = calculate(prepareData(inputString));
                    if(!result.isEmpty()) {
-                       writeResult(result);
+
+                       //sort result
+                       TreeMap<String,Integer> sortedResult
+                               = new TreeMap<String,Integer>(new ResultComparator(result));
+                       sortedResult.putAll(result);
+
+                       writeResult(sortedResult);
                    } else {
                        writeNotice("Data is empty!");
                    }
@@ -96,7 +118,7 @@ public class Main {
         }
 
         /**
-        *
+        * calculate data count
         * @param logins
         * @return
         */
@@ -118,7 +140,29 @@ public class Main {
         }
     }
 
+    /**
+     * comparator for calculated result
+     */
+    public static class ResultComparator implements Comparator<String> {
+        private Map<String, Integer> result;
 
+        public ResultComparator(Map<String, Integer> result) {
+            this.result = result;
+        }
+
+        public int compare(String a, String b) {
+            int cntA = result.get(a);
+            int cntB = result.get(b);
+
+            if(cntA == cntB) {
+                return a.compareTo(b);
+            } else if (cntA > cntB) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
 
 	public static interface InputStreamProvider {
 		InputStream provide();
@@ -139,13 +183,6 @@ public class Main {
 		@Override
 		public OutputStream provide() {
 			return System.out;
-		}
-	}
-
-	public static class FileInputStreamProvider implements InputStreamProvider {
-		@Override
-		public InputStream provide() {
-			return System.in;
 		}
 	}
 }
